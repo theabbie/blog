ff44c08fa95caf97e4221957beebb7aacb9106a3
abhishek <abhishek7gg7@gmail.com>
2019-09-08 22:40:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const app = require('express')();
 const axios = require("axios");
 
 app.get('/*', async function(req, res) {
-var data = await axios("https://typi.tk/?url=https://www.google.com/images?q=Try%20to%20program%20the%20PCI%20card,%20maybe%20it%20will%20program%20the%20mobile%20bus!&sel=img&attribs=src&static=true")
-res.json(data.data)
+var list = await axios("https://typi.tk/?url=https://github.com/theabbie/awto/tree/gh-pages/files&sel=.list-item&attribs=class&t=1")
+res.json(data.data.map(x=>x.text))
 })
 
 app.listen(process.env.port)
