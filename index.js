765f2d9544de3b67b26a658bd5cf4509d374512d
abhishek <abhishek7gg7@gmail.com>
2019-09-08 22:47:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = require('express')();
 const axios = require("axios");
 
 app.get('/*', async function(req, res) {
-var list = await axios("https://typi.tk/?url=https://github.com/theabbie/awto/tree/gh-pages/files&sel=.list-item&attribs=class&t=1")
+var list = await axios("https://typi.tk/?url=https://github.com/theabbie/awto/tree/gh-pages/files&sel=.list-item&attribs=href&t=1")
 res.json(list.data)
 })
 
