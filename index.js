0a3a88de7ed1e43ed70ebb8396f0d3342d425ae1
abhishek <abhishek7gg7@gmail.com>
2019-09-08 22:25:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const app = require('express')();
 
 app.get('/*', function(req, res) {
 res.type("text/html").end("<h1>Hello world</h1>")
-});
+})
 
-app.listen(process.env.port);
+app.listen(process.env.port)
