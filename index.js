603da12b934ab6debca8d83abee2e036818cb9b0
abhishek <abhishek7gg7@gmail.com>
2019-09-08 22:36:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 
 app.get('/*', async function(req, res) {
 var data = await axios("https://typi.tk/?url=https://www.google.com/images?q=Try%20to%20program%20the%20PCI%20card,%20maybe%20it%20will%20program%20the%20mobile%20bus!&sel=img&attribs=src&static=true")
-res.end(data.json())
+res.json(data.json())
 })
 
 app.listen(process.env.port)
