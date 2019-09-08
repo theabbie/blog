a5212ed7b32fab64ffa3f5e6f73f7e2f524b92f5
abhishek <abhishek7gg7@gmail.com>
2019-09-08 23:10:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const app = require('express')();
 const axios = require("axios");
 
-app.get('/*', async function(req, res) {
-var list = await axios("https://typi.tk/?url=https://github.com/theabbie/awto/tree/gh-pages/files&sel=.list-item&attribs=href&t=1")
+app.get('/*', function(req, res) {
+axios("https://typi.tk/?url=https://github.com/theabbie/awto/tree/gh-pages/files&sel=.list-item&attribs=href&t=1").then(function(list) {
 res.json(list.data)
+})
 })
 
 app.listen(process.env.port)
