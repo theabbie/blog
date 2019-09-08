4c70171615899c5a4273732eb553b6bd7a3a75b0
abhishek <abhishek7gg7@gmail.com>
2019-09-08 22:51:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 
 app.get('/*', async function(req, res) {
 var list = await axios("https://typi.tk/?url=https://github.com/theabbie/awto/tree/gh-pages/files&sel=.list-item&attribs=href&t=1")
-res.json(list.json())
+res.json(list.data)
 })
 
 app.listen(process.env.port)
