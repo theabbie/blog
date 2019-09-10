358499d92cd9c6b726e97086f804fb66d8049fb5
abhishek <abhishek7gg7@gmail.com>
2019-09-10 14:44:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -84,11 +84,11 @@ ${repeat("<li class='list-group-item'><a href='/||'>||</a></li>",list)}
 </div>
 <div class="col-sm-7">
 <nav aria-label="breadcrumb">
-  <ol class="breadcrumb">
+  <ul class="breadcrumb">
     <li class="breadcrumb-item"><a href="#">Home</a></li>
     <li class="breadcrumb-item"><a href="#">Library</a></li>
     <li class="breadcrumb-item active" aria-current="page">Data</li>
-  </ol>
+  </ul>
 </nav>
 ${content.data}
 </div>
