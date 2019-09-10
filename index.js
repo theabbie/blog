8653af4c581c1ccec6255a6945c24b50bf945fbb
abhishek <abhishek7gg7@gmail.com>
2019-09-10 14:06:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -67,6 +67,28 @@ src: url('https://cdn.jsdelivr.net/gh/theabbie/example@gh-pages/files/kirvy.otf'
 * {font-family: kirvy;}
 body {margin: 0 0 0 0;}
 a {text-decoration: none; color: black;}
+ul.breadcrumb {
+  padding: 10px 16px;
+  list-style: none;
+  background-color: #eee;
+}
+ul.breadcrumb li {
+  display: inline;
+  font-size: 18px;
+}
+ul.breadcrumb li+li:before {
+  padding: 8px;
+  color: black;
+  content: "/\00a0";
+}
+ul.breadcrumb li a {
+  color: #0275d8;
+  text-decoration: none;
+}
+ul.breadcrumb li a:hover {
+  color: #01447e;
+  text-decoration: underline;
+}
 </style>
 </head>
 <body>
@@ -83,6 +105,12 @@ ${repeat("<li class='list-group-item'><a href='/||'>||</a></li>",list)}
 </ul>
 </div>
 <div class="col-sm-7">
+<ul class="breadcrumb">
+  <li><a href="#">Home</a></li>
+  <li><a href="#">Pictures</a></li>
+  <li><a href="#">Summer 15</a></li>
+  <li>Italy</li>
+</ul>
 ${content.data}
 </div>
 <div class="col-sm-3">
